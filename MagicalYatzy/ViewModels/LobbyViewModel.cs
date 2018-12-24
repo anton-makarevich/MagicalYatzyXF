2353eda1f0edaf9e0b32822b16325c8d5f2e1a5b
Anton Makarevich <amakarevich@mobiquityinc.com>
2018-12-24 13:48:10 +0100

    Add lobbyviewmodel placeholder and navigation to it

---
+++ MagicalYatzy/ViewModels/LobbyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Sanet.MagicalYatzy.ViewModels.Base;
+
+namespace Sanet.MagicalYatzy.ViewModels
+{
+    public class LobbyViewModel: BaseViewModel
+    {
+        public LobbyViewModel()
+        {
+        }
+    }
+}
