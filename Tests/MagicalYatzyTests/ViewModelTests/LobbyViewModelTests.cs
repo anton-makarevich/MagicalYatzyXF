6d344c24453056a28349fc6aeb23ba396a7c0edc
Anton Makarevich <amakarevich@mobiquityinc.com>
2018-12-25 01:49:24 +0100

    Add tests file for lobby viewmodel tests

---
+++ Tests/MagicalYatzyTests/ViewModelTests/LobbyViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Sanet.MagicalYatzy.Models.Game;
+using Sanet.MagicalYatzy.ViewModels;
+using NSubstitute;
+using Xunit;
+namespace MagicalYatzyTests.ViewModelTests
+{
+    public class LobbyViewModelTests
+    {
+        private readonly IDicePanel _dicePanelMock;
+        private readonly LobbyViewModel _sut;
+
+        public LobbyViewModelTests()
+        {
+            _dicePanelMock = Substitute.For<IDicePanel>();
+            _sut = new LobbyViewModel(_dicePanelMock);
+        }
+
+        [Fact]
+        public void LobbyViewModelHasDicePanel()
+        {
+            Assert.NotNull(_sut.DicePanel);
+        }
+    }
+}
