1b1fe570f9b1bbbd3047d1a0c4483eb02bfed07a
Anton Makarevich <amakarevich@mobiquityinc.com>
2018-12-28 11:07:47 +0100

    Remove unused struct

---
+++ Tests/MagicalYatzyTests/ModelTests/Common/PointTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Sanet.MagicalYatzy.Models.Common;
+
+namespace MagicalYatzyTests.ModelTests.Common
+{
+    public class PointTests
+    {
+        [Fact]
+        public void ItIsPossibleToSumPoints()
+        {
+            Point point1 = new Point(20, 30);
+            Point point2 = new Point(30, 40);
+
+            var resultinPoint = point1 + point2;
+
+            Assert.Equal(20 + 30, resultinPoint.X);
+            Assert.Equal(30 + 40, resultinPoint.Y);
+        }
+    }
+}
