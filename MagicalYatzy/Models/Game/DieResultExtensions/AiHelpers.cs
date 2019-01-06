cf73e63f89bb7dccce8970c498710ff247118be4
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-01-06 22:39:43 +0100

    Refactor DiceResult AI helpers

--- MagicalYatzy/Models/Game/DieResultExtensions/AiHelpers.cs
+++ MagicalYatzy/Models/Game/DieResultExtensions/AiHelpers.cs
@@ -5,53 +5,45 @@ namespace Sanet.MagicalYatzy.Models.Game.DieResultExtensions
         public static int NumPairs(this DieResult result)
         {
 
-            int[] iOccur = new int[7];
-            int bPair = 0;
+            var occurrences = new int[7];
+            var numberOfPairs = 0;
 
-            foreach (int res in result.DiceResults)
+            foreach (var diceValue in result.DiceResults)
             {
-                iOccur[res] += 1;
+                occurrences[diceValue] += 1;
             }
 
-            for (int i = 0; i <= 6; i++)
+            for (var i = 0; i <= 6; i++)
             {
-                if (iOccur[i] > 1)
+                if (occurrences[i] > 1)
                 {
-                    bPair++;
+                    numberOfPairs++;
                 }
-                //if (iOccur[i] > 3)
-                //{
-                //    bPair++;
-                //}
-                //if (iOccur[i] > 5)
-                //{
-                //    bPair++;
-                //}
             }
 
-
-            return bPair;
+            return numberOfPairs;
         }
         
-        public static int XInRow(this DieResult result, ref int count)
+        public static (int firstValue,int numberOfValuesInRow) XInRow(this DieResult result)
         {
-            
-            int[] iOccur = new int[7];
-            count = 3;
-            foreach (int res in result.DiceResults)
+            var occurrences = new int[7];
+            var count = 3;
+            foreach (var res in result.DiceResults)
             {
-                iOccur[res] += 1;
+                occurrences[res] += 1;
             }
-            for (int i = 1;i<5;i++)
-                if (iOccur[i] >= 1 & iOccur[i+1] >= 1 & iOccur[i+2] >= 1)
+            for (var i = 1;i<5;i++)
+                if (occurrences[i] >= 1 & occurrences[i+1] >= 1 & occurrences[i+2] >= 1)
                 {
-                    if (i < 4 && iOccur[i + 3] >= 1)
-                        count = 4;
-                    return i;
+                    if (i >= 4 || occurrences[i + 3] < 1) return (i, count);
+                    count = 4;
+                    if (i < 3 && occurrences[i + 4] >= 1)
+                        count = 5;
 
+                    return (i,count);
                 }
 
-            return 0;
+            return (0,0);
         }
     }
 }
---
+++ Tests/MagicalYatzyTests/ModelTests/Game/DieResultExtensions/AiHelpersTests.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Sanet.MagicalYatzy.Models.Game;
+using Sanet.MagicalYatzy.Models.Game.DieResultExtensions;
+using Xunit;
+
+namespace MagicalYatzyTests.ModelTests.Game.DieResultExtensions
+{
+    public class AiHelpersTests
+    {
+        [Fact]
+        public void NumPairsReturnsCorrectAmountOfPairs()
+        {
+            var sut = new DieResult {DiceResults = new List<int> {1, 2, 1, 2, 1}};
+            var result = sut.NumPairs();
+            Assert.Equal(2,result);
+            
+            sut = new DieResult {DiceResults = new List<int> {1, 3, 1, 2, 1}};
+            result = sut.NumPairs();
+            Assert.Equal(1,result);
+            
+            sut = new DieResult {DiceResults = new List<int> {1, 3, 6, 2, 5}};
+            result = sut.NumPairs();
+            Assert.Equal(0,result);
+        }
+
+        [Fact]
+        public void XInRowReturnsFirstValueAndAmountOfValuesInRow()
+        {
+            var sut = new DieResult {DiceResults = new List<int> {3, 2, 4, 2, 4}};
+            var (firstValue, numberOfValuesInRow) = sut.XInRow();
+            Assert.Equal(2,firstValue);
+            Assert.Equal(3,numberOfValuesInRow);
+            
+            sut = new DieResult {DiceResults = new List<int> {3, 2, 4, 2, 1}};
+            (firstValue, numberOfValuesInRow) = sut.XInRow();
+            Assert.Equal(1,firstValue);
+            Assert.Equal(4,numberOfValuesInRow);
+            
+            sut = new DieResult {DiceResults = new List<int> {3, 5, 4, 2, 6}};
+            (firstValue, numberOfValuesInRow) = sut.XInRow();
+            Assert.Equal(2,firstValue);
+            Assert.Equal(5,numberOfValuesInRow);
+        }
+    }
+}
