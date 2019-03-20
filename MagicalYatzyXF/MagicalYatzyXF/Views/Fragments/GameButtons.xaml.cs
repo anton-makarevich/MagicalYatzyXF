ba4e4d5a1aa37156bc724afcb322f696da47a5a6
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-03-20 15:06:47 +0100

    Reuse gamebuttons between wide and narrow game pages

---
+++ MagicalYatzyXF/MagicalYatzyXF/Views/Fragments/GameButtons.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Sanet.MagicalYatzy.XF.Views.Fragments
+{
+    public partial class GameButtons
+    {
+        public GameButtons()
+        {
+            InitializeComponent();
+        }
+    }
+}
