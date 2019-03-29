b20e860e9deeb4ffbe04bcfce02cf6b7d3418cec
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-03-29 08:15:02 +0100

    Add test dice generator for kniffels only

---
+++ MagicalYatzy/Models/Game/DiceGenerator/AllOfKindTestGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Sanet.MagicalYatzy.Models.Game.DiceGenerator
+{
+    public class AllOfKindTestGenerator : IDiceGenerator
+    {
+        private readonly Random _random = new Random();
+        public int GetNextDiceResult(int[] previousResults = null)
+        {
+            if (previousResults != null && previousResults.Any())
+                return previousResults.First();
+            return _random.Next(1, 7);
+        }
+    }
+}
