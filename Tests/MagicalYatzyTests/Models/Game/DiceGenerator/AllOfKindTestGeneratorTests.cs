5c3f027d8c798b26d27ccf8d83fb5f79babb85ac
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-06-10 23:26:13 +0200

    Add missing test for dicegenerator happy flow

--- Tests/MagicalYatzyTests/Models/Game/DiceGenerator/AllOfKindTestGeneratorTests.cs
+++ Tests/MagicalYatzyTests/Models/Game/DiceGenerator/AllOfKindTestGeneratorTests.cs
@@ -11,10 +11,22 @@ namespace MagicalYatzyTests.Models.Game.DiceGenerator
             var fixedDice = new[] { 1 };
             var sut = new AllOfKindTestGenerator();
 
-            for(int i = 0; i<10;i++)
+            for(var i = 0; i<10;i++)
             {
                 var value = sut.GetNextDiceResult(fixedDice);
                 Assert.Equal(1, value);
+            }
+        }
+        
+        [Fact]
+        public void GeneratesValidDiceValue()
+        {
+            var sut = new AllOfKindTestGenerator();
+
+            for(var i = 0; i<10;i++)
+            {
+                var value = sut.GetNextDiceResult();
+                Assert.True(value>0 && value < 7);
             }
         }
     }
