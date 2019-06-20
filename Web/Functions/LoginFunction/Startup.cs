5be2509b5a5d63601e81e1adde4ca3541f5c7352
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-06-20 10:04:27 +0200

    Add fake DI for login function

---
+++ Web/Functions/LoginFunction/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Sanet.MagicalYatzy.Web.Functions.Login;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Sanet.MagicalYatzy.Web.Functions.Login
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IGetHello, GetHelloClass>();
+        }
+    }
+
+    public interface IGetHello
+    {
+        string GetHello(string name);
+    }
+
+    public class GetHelloClass : IGetHello
+    {
+        public string GetHello(string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+}
