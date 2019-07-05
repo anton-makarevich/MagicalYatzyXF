2c4081cc34a1b21a1fc498c34c36816f8793af5c
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-07-05 11:16:21 +0200

    Comment yet unused service code

--- Web/Functions/ScoreSaverFunction/Services/AzureLeaderBoardService.cs
+++ Web/Functions/ScoreSaverFunction/Services/AzureLeaderBoardService.cs
@@ -8,20 +8,21 @@ namespace Sanet.MagicalYatzy.Web.Functions.ScoreSaver.Services
 {
     public class AzureLeaderBoardService:ILeaderBoardService
     {
-        const string TableName = "ScoresTable";
-        private readonly string _connectionString = Environment.GetEnvironmentVariable("TableConnection");
-        CloudStorageAccount _storageAccount; 
-        CloudTableClient tableClient; 
-        CloudTable customerTable;
+//        const string TableName = "ScoresTable";
+//        private readonly string _connectionString = Environment.GetEnvironmentVariable("TableConnection");
+//        CloudStorageAccount _storageAccount; 
+//        CloudTableClient tableClient; 
+//        CloudTable customerTable;
 
         public AzureLeaderBoardService()
         {
-            _storageAccount = CloudStorageAccount.Parse(_connectionString); 
+            //_storageAccount = CloudStorageAccount.Parse(_connectionString); 
         }
         
         public Task<string> SaveScoreAsync(PlayerScore score)
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            return null;
         }
     }
 }
