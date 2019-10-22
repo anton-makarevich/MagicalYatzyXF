17181e0b94db4f55451a48832e7dfe58e26a0875
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-10-22 17:17:43 +0200

    Dont make errorCode to be optional

--- MagicalYatzyDto/Responses/Base/ResponseBase.cs
+++ MagicalYatzyDto/Responses/Base/ResponseBase.cs
@@ -3,6 +3,6 @@ namespace Sanet.MagicalYatzy.Dto.Responses.Base
     public abstract class ResponseBase
     {
         public string? Message { get; set; }
-        public int? ErrorCode { get; set; }
+        public int ErrorCode { get; set; }
     }
 }
