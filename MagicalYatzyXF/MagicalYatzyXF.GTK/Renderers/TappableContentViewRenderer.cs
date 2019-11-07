09ec81e61748e5bd4e24c95516ba3f28838bfa82
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-11-07 12:53:02 +0100

    Add a basic empty renderer for tappableview

---
+++ MagicalYatzyXF/MagicalYatzyXF.GTK/Renderers/TappableContentViewRenderer.cs
@@ -0,0 +1,35 @@
+using Gtk;
+using Sanet.MagicalYatzy.Xf.Gtk.Renderers;
+using Sanet.MagicalYatzy.Xf.Views.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+[assembly: ExportRenderer(typeof(TappableContentView), typeof(TappableContentViewRenderer))]
+
+namespace Sanet.MagicalYatzy.Xf.Gtk.Renderers
+{
+    public class TappableContentViewRenderer :ViewRenderer<TappableContentView, Fixed>
+    {
+        private Fixed _fixed;
+        
+        protected override void OnElementChanged(ElementChangedEventArgs<TappableContentView> e)
+        {
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    if(_fixed == null)
+                    {
+                        _fixed = new Fixed();
+                    }
+
+                    SetNativeControl(_fixed);
+                }
+                
+            }
+
+            base.OnElementChanged(e);
+        }
+    }
+}
