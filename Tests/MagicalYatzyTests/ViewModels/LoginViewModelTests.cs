f6169aa93014ec93f3b5c33c212e19f35c9ca04d
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-04 00:00:23 +0200

    Bump net for core unit tests

--- Tests/MagicalYatzyTests/ViewModels/LoginViewModelTests.cs
+++ Tests/MagicalYatzyTests/ViewModels/LoginViewModelTests.cs
@@ -5,71 +5,71 @@ using Sanet.MagicalYatzy.Models.Game;
 using Sanet.MagicalYatzy.Services.Game;
 using Sanet.MagicalYatzy.Services.Navigation;
 using Sanet.MagicalYatzy.ViewModels;
+using Sanet.MVVM.Core.Services;
 using Xunit;
 
-namespace MagicalYatzyTests.ViewModels
+namespace MagicalYatzyTests.ViewModels;
+
+public class LoginViewModelTests
 {
-    public class LoginViewModelTests
+    private readonly IPlayerService _playerServiceMock = Substitute.For<IPlayerService>();
+    private readonly INavigationService _navigationServiceMock = Substitute.For<INavigationService>();
+
+    private readonly IPlayer _playerStub = Substitute.For<IPlayer>();
+        
+    private readonly LoginViewModel _sut;
+
+    public LoginViewModelTests()
     {
-        private readonly IPlayerService _playerServiceMock = Substitute.For<IPlayerService>();
-        private readonly INavigationService _navigationServiceMock = Substitute.For<INavigationService>();
+        _sut = new LoginViewModel(_playerServiceMock);
+        _sut.SetNavigationService(_navigationServiceMock);
+    }
 
-        private readonly IPlayer _playerStub = Substitute.For<IPlayer>();
+    [Fact]
+    public void SuccessfulLoginCallsBackNavigation()
+    {
+        _playerServiceMock.LoginAsync(PlayerServiceTests.TestUserName, PlayerServiceTests.TestUserPassword).Returns(Task.FromResult(_playerStub));
+
+        _sut.NewUsername = PlayerServiceTests.TestUserName;
+        _sut.NewPassword = PlayerServiceTests.TestUserPassword;
+
+        _sut.LoginCommand.Execute(null);
+
+        _navigationServiceMock.Received().CloseAsync();
+    }
         
-        private readonly LoginViewModel _sut;
-
-        public LoginViewModelTests()
+    [Fact]
+    public void SuccessfulLoginCallsBackNavigationAndProvidesLoggedInPlayerAsResultIfItIsExpected()
+    {
+        _sut.ExpectsResult = true;
+        var onResultCalledCount = 0;
+        _sut.OnResult += (sender, o) =>
         {
-            _sut = new LoginViewModel(_playerServiceMock);
-            _sut.SetNavigationService(_navigationServiceMock);
-        }
-
-        [Fact]
-        public void SuccessfulLoginCallsBackNavigation()
-        {
-            _playerServiceMock.LoginAsync(PlayerServiceTests.TestUserName, PlayerServiceTests.TestUserPassword).Returns(Task.FromResult(_playerStub));
-
-            _sut.NewUsername = PlayerServiceTests.TestUserName;
-            _sut.NewPassword = PlayerServiceTests.TestUserPassword;
-
-            _sut.LoginCommand.Execute(null);
-
-            _navigationServiceMock.Received().CloseAsync();
-        }
-        
-        [Fact]
-        public void SuccessfulLoginCallsBackNavigationAndProvidesLoggedInPlayerAsResultIfItIsExpected()
-        {
-            _sut.ExpectsResult = true;
-            var onResultCalledCount = 0;
-            _sut.OnResult += (sender, o) =>
-            {
-                onResultCalledCount++;
-                Assert.Equal(_playerStub, o);
-            }; 
+            onResultCalledCount++;
+            Assert.Equal(_playerStub, o);
+        }; 
             
-            _playerServiceMock.LoginAsync(PlayerServiceTests.TestUserName, PlayerServiceTests.TestUserPassword).Returns(Task.FromResult(_playerStub));
+        _playerServiceMock.LoginAsync(PlayerServiceTests.TestUserName, PlayerServiceTests.TestUserPassword).Returns(Task.FromResult(_playerStub));
 
-            _sut.NewUsername = PlayerServiceTests.TestUserName;
-            _sut.NewPassword = PlayerServiceTests.TestUserPassword;
+        _sut.NewUsername = PlayerServiceTests.TestUserName;
+        _sut.NewPassword = PlayerServiceTests.TestUserPassword;
 
-            _sut.LoginCommand.Execute(null);
+        _sut.LoginCommand.Execute(null);
 
-            _navigationServiceMock.Received().CloseAsync();
-            Assert.Equal(1,onResultCalledCount);
-        }
+        _navigationServiceMock.Received().CloseAsync();
+        Assert.Equal(1,onResultCalledCount);
+    }
 
-        [Fact] 
-        public void CloseCommandShouldCallBackNavigation()
-        {
-            _sut.CloseCommand.Execute(null);
-            _navigationServiceMock.Received().CloseAsync();
-        }
+    [Fact] 
+    public void CloseCommandShouldCallBackNavigation()
+    {
+        _sut.CloseCommand.Execute(null);
+        _navigationServiceMock.Received().CloseAsync();
+    }
 
-        [Fact]
-        public void CloseImageHasCorrectValue()
-        {
-            Assert.Equal("close.png", _sut.CloseImage);
-        }
+    [Fact]
+    public void CloseImageHasCorrectValue()
+    {
+        Assert.Equal("close.png", _sut.CloseImage);
     }
 }
