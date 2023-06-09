f444861e7d15520b80b90e25f2b5b465876f8948
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-09 09:20:06 +0200

    Register minimal set of services

--- MagicalYatzyAUI/MagicalYatzyAUI/App.axaml.cs
+++ MagicalYatzyAUI/MagicalYatzyAUI/App.axaml.cs
@@ -1,9 +1,12 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
 using Sanet.MagicalYatzy.Avalonia.DependencyInjection;
 using Sanet.MagicalYatzy.Avalonia.Views;
+using Sanet.MagicalYatzy.ViewModels;
+using Sanet.MVVM.Navigation.Avalonia.Services;
 using MainWindow = Sanet.MagicalYatzy.Avalonia.Views.MainWindow;
 
 namespace Sanet.MagicalYatzy.Avalonia;
@@ -17,15 +20,26 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var services = Resources[Sanet.MVVM.DI.Avalonia.Extensions.AppBuilderExtensions.ServiceCollectionResourceKey] as IServiceCollection;
+        var services = Resources[MVVM.DI.Avalonia.Extensions.AppBuilderExtensions.ServiceCollectionResourceKey] as IServiceCollection;
         services?.RegisterServices();
         services?.RegisterViewModels();
-        
+        if (services == null)
+        {
+            throw new Exception("Services are not initialized");
+        }
+        var serviceProvider = services.BuildServiceProvider();
+       
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            var navigationService = new NavigationService(desktop, serviceProvider);
+            RegisterViews(navigationService);
+            var vm = navigationService.GetViewModel<MainMenuViewModel>();
             desktop.MainWindow = new MainWindow
             {
-                //DataContext = new MainMenuViewModel()
+                Content = new MainMenuView()
+                {
+                    ViewModel = vm
+                }
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
@@ -37,5 +51,10 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void RegisterViews(NavigationService navigationService)
+    {
+        navigationService.RegisterViewModels(typeof(MainMenuViewModel), typeof(MainMenuView));
     }
 }
