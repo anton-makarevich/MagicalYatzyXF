d8934ea9c7a05cc5eab286c387338429b9ec8b8a
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-21 22:53:20 +0200

    Add initial files for menu

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Converter/ImageNameConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+
+namespace Sanet.MagicalYatzy.Avalonia.Converter;
+
+public class ImageNameConverter : IValueConverter
+{
+    public object? Convert(
+        object? value, Type targetType, 
+        object? parameter,
+        System.Globalization.CultureInfo culture)
+    {
+        if (value is not string assetPath) return null;
+        var assetsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets");
+        var imagePath = Path.Combine(assetsDirectory, assetPath);
+
+        return File.Exists(imagePath) 
+            ? new Bitmap(imagePath) 
+            : null;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter,
+        System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Converter/StringUppercasedConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Avalonia.Data.Converters;
+
+namespace Sanet.MagicalYatzy.Avalonia.Converter;
+
+public class StringUppercasedConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        return value is not string stringValue 
+            ? string.Empty 
+            : stringValue.ToUpper();
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter,
+        System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
