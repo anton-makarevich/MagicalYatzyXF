d08c5c21372effb41d3435fd318eea26eb67749c
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-23 17:10:36 +0200

    Add wide lobby view template

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Converter/AdaptiveOrientationConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Layout;
+
+namespace Sanet.MagicalYatzy.Avalonia.Converter;
+
+public class AdaptiveOrientationConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not Size size)
+            return Orientation.Vertical; // Default orientation if the value is not a Size object
+        var isHorizontal = size.Width > size.Height;
+        return isHorizontal ? Orientation.Horizontal : Orientation.Vertical;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/Lobby/LobbyViewWide.axaml.cs
@@ -0,0 +1,12 @@
+using Sanet.MagicalYatzy.Avalonia.Views.Base;
+using Sanet.MagicalYatzy.ViewModels;
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.Lobby;
+
+public partial class LobbyViewWide : DicePanelView<LobbyViewModel>
+{
+    public LobbyViewWide()
+    {
+        InitializeComponent();
+    }
+}
