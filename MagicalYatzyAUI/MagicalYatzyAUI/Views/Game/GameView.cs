78109e385866bf37881686090ccf545df0b67c6b
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-23 08:50:07 +0200

    Copy over the base gamepage

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/Game/GameView.cs
@@ -0,0 +1,31 @@
+using Sanet.MagicalYatzy.ViewModels;
+using Sanet.MVVM.Views.Avalonia;
+using Sanet.MagicalYatzy.Avalonia.Controls.Game;
+
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.Game;
+
+public abstract class GameView: BaseView<GameViewModel>
+{
+    protected DicePanelControl? DicePanelView;
+    protected override void OnLoaded()
+    {
+        InitDicePanel();
+        base.OnLoaded();
+    }
+
+    protected virtual void InitDicePanel()
+    {
+        if (DicePanelView != null)
+            return;
+
+        if (ViewModel == null) return;
+        DicePanelView = new DicePanelControl(true)
+        {
+            DicePanel = ViewModel.DicePanel
+        };
+
+        ViewModel.DicePanel.DiceCount = 5;
+        ViewModel.DicePanel.RollDelay = 30;
+    }
+}
