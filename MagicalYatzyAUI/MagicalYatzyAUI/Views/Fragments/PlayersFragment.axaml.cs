43a971d0ecc0ffdb188d5add0d3e4873e3894441
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-24 17:24:59 +0200

    Convert players fragment from xf

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/Fragments/PlayersFragment.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.Fragments;
+
+public partial class PlayersFragment : UserControl
+{
+    public PlayersFragment()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
