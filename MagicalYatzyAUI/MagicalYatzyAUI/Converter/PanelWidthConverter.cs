401aa390e1a8afea2f46e4b147de972c1993c32a
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-24 22:42:39 +0200

    Use shared control for all the menus instead of cells

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Converter/PanelWidthConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Layout;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace Sanet.MagicalYatzy.Avalonia.Converter;
+
+public class PanelWidthConverter : IValueConverter
+{
+    public object? Convert(
+        object? value, Type targetType, 
+        object? parameter,
+        System.Globalization.CultureInfo culture)
+    {
+        if (value is not Size size)
+        {
+            return 600;
+        }
+        return Math.Min(size.Width, 600);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter,
+        System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/TemplatedControls/MainMenuItem.axaml.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.TemplatedControls;
+
+public class MainMenuItem : TemplatedControl
+{
+    public static readonly StyledProperty<string> CaptionProperty = AvaloniaProperty.Register<MainMenuItem, string>(
+        nameof(Caption),"Caption");
+
+    public string Caption
+    {
+        get => GetValue(CaptionProperty);
+        set => SetValue(CaptionProperty, value);
+    }
+
+    public static readonly StyledProperty<string> DescriptionProperty = AvaloniaProperty.Register<MainMenuItem, string>(
+        nameof(Description), "description");
+                                                     
+    public string Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+    public static readonly StyledProperty<string> ImageSourceProperty = AvaloniaProperty.Register<MainMenuItem, string>(
+        nameof(ImageSource),"SanetDice.png");
+
+    public string ImageSource
+    {
+        get => GetValue(ImageSourceProperty);
+        set => SetValue(ImageSourceProperty, value);
+    }
+}
