b606ff791253c147fa5ed3a4114a15d6b879dd22
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-24 20:32:05 +0200

    Convert rules fragment from xf

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/Cells/RuleCell.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.Cells;
+
+public partial class RuleCell : UserControl
+{
+    public RuleCell()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/Fragments/RulesFragment.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.Fragments;
+
+public partial class RulesFragment : UserControl
+{
+    public RulesFragment()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
