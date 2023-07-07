4af5a6548d8b64180fb0b167132e1b480152eaa6
Anton Makarevich <anton.makarevich@hotmail.com>
2023-07-08 00:22:26 +0200

    Extract roll results to a separate fragment

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/Fragments/RollResultsPanel.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.Markup.Xaml;
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.Fragments;
+
+public partial class RollResultsPanel : UserControl
+{
+    public RollResultsPanel()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+    
+    public static readonly StyledProperty<Orientation> OrientationProperty = AvaloniaProperty.Register<GameButtons, Orientation>(
+        nameof(Orientation));
+
+    public Orientation Orientation
+    {
+        get => GetValue(OrientationProperty);
+        set => SetValue(OrientationProperty, value);
+    }
+}
