5314b931cf5e4413e97b566ae9dd7f2dfe4d63ae
Anton Makarevich <anton.makarevich@hotmail.com>
2023-07-08 21:44:52 +0200

    Add test for randomizer

---
+++ Tests/MagicalYatzyTests/Models/Game/DiceGenerator/RandomValueGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Sanet.MagicalYatzy.Models.Game.DiceGenerator;
+using Xunit;
+
+namespace MagicalYatzyTests.Models.Game.DiceGenerator;
+
+public class RandomValueGeneratorTests
+{
+    private readonly RandomValueGenerator _sut = new();
+
+    [Fact]
+    public void GeneratesValueInARange()
+    {
+        const int min = 1;
+        const int max = 6;
+        var value = _sut.Next(min, max);
+        value.Should().BeGreaterOrEqualTo(min);
+        value.Should().BeLessThan(max);
+    }
+}
