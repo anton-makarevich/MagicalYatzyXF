b6aba45af13825ea705c6601fb1184a1ed395c4f
Anton Makarevich <anton.makarevich@hotmail.com>
2023-07-11 21:50:40 +0200

    Create a toolbar control

---
+++ MagicalYatzyAUI/MagicalYatzyAUI/Views/TemplatedControls/ToolBar.axaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace Sanet.MagicalYatzy.Avalonia.Views.TemplatedControls;
+
+public class ToolBar : TemplatedControl
+{
+    public static readonly StyledProperty<ICommand> LeftCommandProperty =
+        AvaloniaProperty.Register<ToolBar, ICommand>(nameof(LeftCommand));
+
+    public static readonly StyledProperty<string> LeftIconProperty = AvaloniaProperty.Register<ToolBar, string>(
+        nameof(LeftIcon));
+
+    public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<ToolBar, string>(
+        nameof(Title));
+
+    public static readonly StyledProperty<ICommand> RightCommandProperty = AvaloniaProperty.Register<ToolBar, ICommand>(
+        nameof(RightCommand));
+
+    public static readonly StyledProperty<string> RightIconProperty = AvaloniaProperty.Register<ToolBar, string>(
+        nameof(RightIcon));
+    
+    public string RightIcon
+    {
+        get => GetValue(RightIconProperty);
+        set => SetValue(RightIconProperty, value);
+    }
+    
+    public ICommand RightCommand
+    {
+        get => GetValue(RightCommandProperty);
+        set => SetValue(RightCommandProperty, value);
+    }
+    
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public string LeftIcon
+    {
+        get => GetValue(LeftIconProperty);
+        set => SetValue(LeftIconProperty, value);
+    }
+
+    public ICommand LeftCommand
+    {
+        get => GetValue(LeftCommandProperty);
+        set => SetValue(LeftCommandProperty, value);
+    }
+
+    public bool HasLeftButton => !string.IsNullOrEmpty(LeftIcon);
+    public bool HasRightButton => !string.IsNullOrEmpty(RightIcon);
+}
