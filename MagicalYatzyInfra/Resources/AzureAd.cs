01d1dff70b4785a9b7011726a719f13d99dcc9ab
Anton Makarevich <anton.makarevich@hotmail.com>
2024-03-22 21:36:42 +0100

    feat: start pulumi project with b2c tenant

---
+++ MagicalYatzyInfra/Resources/AzureAd.cs
@@ -0,0 +1,30 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.AzureActiveDirectory;
+using Pulumi.AzureNative.AzureActiveDirectory.Inputs;
+using Pulumi.AzureNative.Resources;
+
+namespace MagicalYatzy.Infra.Resources;
+
+public class AzureAd
+{
+    public AzureAd(ResourceGroup resourceGroup)
+    {
+        // Create an Azure AD B2C Tenant
+        var tenant = new B2CTenant("MyTenant", new B2CTenantArgs
+        {
+            CountryCode = "NL",
+            DisplayName = "MagicalYatzy B2C Tenant",
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new B2CResourceSKUArgs
+            {
+                Name = "Standard",
+                Tier = "A0",
+            }
+        });
+
+        TenantId = tenant.TenantId;
+    }
+
+    [Output]
+    public Output<string?> TenantId { get; private set; }
+}
