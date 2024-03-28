ecee859d0150ebf303a790f60e26db03f32a0703
Anton Makarevich <amakarevich@mobiquityinc.com>
2024-03-28 16:22:18 +0100

    feat: add OU for my

---
+++ MagicalYatzyInfra/MagicalYatzyInfra.AwsLZ/Program.cs
@@ -0,0 +1,14 @@
+﻿using Pulumi;
+using Pulumi.Aws.Organizations;
+
+return await Deployment.RunAsync(() =>
+{
+    var organization = new Organization("sanetby");
+    // Create an AWS OU
+    var magicalYatzyOU = new OrganizationalUnit("MagicalYatzy", new()
+    {
+        Name = "MagicalYatzy",
+        ParentId = organization.Id,
+    });
+    
+});
