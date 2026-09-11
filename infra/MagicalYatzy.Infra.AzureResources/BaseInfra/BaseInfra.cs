741aac8395d3e00421b429e14ce34a14c66ab997
Anton Makarevich <anton.makarevich@gmail.com>
2026-09-11 12:07:49 +0200

    refactor: restructure the repo

---
+++ infra/MagicalYatzy.Infra.AzureResources/BaseInfra/BaseInfra.cs
@@ -0,0 +1,28 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources.V20240701;
+
+namespace MagicalYatzyAzureResources.BaseInfra;
+
+public class BaseInfra
+{
+    StackReference baseInfraStack = new StackReference("sanetby/MagicalYatzyInfra.AwsLZ/my-aws-lz-dev");
+
+    public ResourceGroup GetResourceGroup()
+    {
+        // Get the resource group name from the output exports of the base infra stack
+        var resourceGroupName = baseInfraStack.GetOutput("resourceGroup")
+            .Apply(resourceGroupName => (string)resourceGroupName);
+
+        // Import the existing resource group
+        var resourceGroup = new ResourceGroup("importedResourceGroup", new ResourceGroupArgs
+        {
+            ResourceGroupName = resourceGroupName,
+        }, new CustomResourceOptions
+        {
+            ImportId = resourceGroupName.Apply(x => x)
+        });
+
+        // Export the resource group name
+        return resourceGroup;
+    }
+}
---
+++ infra/README.md
@@ -0,0 +1,20 @@
+﻿## Azure Infra
+
+To authenticate Pulumi requires a Service Principal.
+Locally it should be saved in `Pulumi.{pulume-stack-name}.yaml` but should not be tracked in Git.
+
+```yaml
+
+```
+
+### Auth/z
+
+#### Local CLI
+
+az login --tenant {main tenant ID}
+Default subscription.
+
+#### GitHub Actions
+Use Service Principal for authentication:
+https://www.pulumi.com/registry/packages/azure-native/installation-configuration/#authenticate-using-a-service-principal
+
